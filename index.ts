64fe9066af7dc435efa2620f09bdd05755c6bd88
Dang Trong Viet <viet113059@gmail.com>
2024-10-26 17:23:12 +0700

    Create project music 2024

--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-console.log("Ok")
+import express, { Express } from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+
+
+import clientRoutes from "./routes/client/index.route";
+
+dotenv.config();
+
+database.connect();
+
+const app : Express = express();
+const port: Number | String = process.env.PORT || 3000;
+
+
+// CLient Routes
+clientRoutes(app);
+
+app.use(express.static("public"));
+
+app.set("views", "./views");
+app.set('view engine', 'pug');
+
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
