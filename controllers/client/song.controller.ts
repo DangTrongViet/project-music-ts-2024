c13b59b4eeaea50bee7cb013194bda993bc1ada1
Dang Trong Viet <viet113059@gmail.com>
2024-11-01 14:51:15 +0700

    Create project music 2024

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -4,6 +4,8 @@ import Topic from "../../models/topic.model";
 import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
 
+import FavoriteSong from "../../models/favorite-song.model";
+
 // [GET] /songs/:slugTopic
 export const list = async (req: Request, res: Response) => {
     const topic = await Topic.findOne({
@@ -17,7 +19,7 @@ export const list = async (req: Request, res: Response) => {
         status: "active",
         deleted: false,
     }).select("avatar title slug like singerId ");
-    
+
     for (const song of songs) {
         const infoSinger = await Singer.findOne({
             _id: song.singerId,
@@ -25,13 +27,121 @@ export const list = async (req: Request, res: Response) => {
             deleted: false
         });
 
-        song["infoSinger"]=infoSinger;
-            
+        song["infoSinger"] = infoSinger;
+
     }
 
 
-    res.render("client/pages/songs/list",{
+    res.render("client/pages/songs/list", {
         pageTitle: topic.title,
-        songs:songs
+        songs: songs
     });
 };
+
+
+// [GET] /songs/detail/:slugSong
+export const detail = async (req: Request, res: Response) => {
+    const slugSong: string = req.params.slugSong;
+
+    const song = await Song.findOne({
+        slug: slugSong,
+        status: "active",
+        deleted: false
+    });
+
+    if(!song || !song.singerId){
+        return;
+    }
+    const singer = await Singer.findOne({
+        _id: song.singerId,
+        status: "active",
+        deleted: false
+    }).select("fullName");
+
+
+    const topic = await Topic.findOne({
+        _id: song.topicId,
+        status: "active",
+        deleted: false
+    }).select("title");
+
+    const favoriteSong = await FavoriteSong.findOne({
+        songId: song.id
+    });
+
+    song["isFavoriteSong"] = favoriteSong ? true : false ;
+
+    res.render("client/pages/songs/detail", {
+        pageTitle: "Chi tiết bài hát",
+        song: song,
+        singer: singer,
+        topic: topic,
+
+    });
+
+
+}
+
+// [PATCH] /songs/like/:typeLike/:idSong
+export const like = async (req: Request, res: Response) => {
+    const idSong: string = req.params.idSong;
+    const typeLike: string = req.params.typeLike;
+   
+   
+    const song =await Song.findOne({
+        _id: idSong,
+        status: "active",
+        deleted: false
+    });
+
+    const newlike: Number = typeLike == "like" ? song.like + 1 : song.like -1;
+
+    await Song.updateOne({
+        _id: idSong
+    }, {
+        like: newlike
+    });
+
+    
+    res.json({
+        code: 200,
+        message: "Thành công!",
+        like: newlike
+    });
+}
+
+// [PATCH] /songs/favorite/:typeFavorite/:idSong
+export const favorite = async (req: Request, res: Response) => {
+    const idSong: string = req.params.idSong;
+    const typeFavorite: string = req.params.typeFavorite;
+    
+    switch (typeFavorite) {
+        case "favorite":
+            const existFavoriteSong = await FavoriteSong.findOne({
+                songId: idSong
+            });
+            if(!existFavoriteSong){
+                const record = new FavoriteSong({
+                    // userId: "",
+                    songId: idSong
+                });
+                await record.save();
+            }
+            break;
+
+        case "unfavorite":
+            await FavoriteSong.deleteOne({
+                songId: idSong
+            });
+            break;
+
+        default:
+            break;
+    }
+
+    
+    res.json({
+        code: 200,
+        message: "Thành công!",
+    });
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,96 @@
-console.log("OK");
+// APlayer
+const aplayer = document.querySelector("#aplayer");
+
+if (aplayer) {
+    let dataSong = aplayer.getAttribute("data-song");
+    let dataSinger = aplayer.getAttribute("data-singer");
+
+    dataSong = JSON.parse(dataSong);
+    dataSinger = JSON.parse(dataSinger);
+    const ap = new APlayer({
+        container: aplayer,
+        audio: [
+            {
+                name: dataSong.title,
+                artist: dataSinger.fullName,
+                url: dataSong.audio,
+                cover: dataSong.avatar
+            },
+        ],
+        autoplay: true,
+        volume: 0.8
+    });
+
+    const avatar = document.querySelector(".singer-detail .inner-avatar");
+
+    ap.on('play', () => {
+        avatar.style.animationPlayState = "running";
+    })
+
+    ap.on('pause', () => {
+        avatar.style.animationPlayState = "paused";
+    })
+
+}
+
+// End Aplayer
+
+// Button Like
+const buttonLike = document.querySelector("[button-like]");
+
+if (buttonLike) {
+    buttonLike.addEventListener("click", () => {
+        const idSong = buttonLike.getAttribute("button-like");
+        const isActive = buttonLike.classList.contains("active");
+
+        const typeLike = isActive ? "dislike" : "like";
+        const link = `/songs/like/${typeLike}/${idSong}`;
+
+        const option = {
+            method: "PATCH"
+        }
+        fetch(link, option)
+            .then(res => res.json())
+            .then(data => {
+                if (data.code == 200) {
+
+                    const span = buttonLike.querySelector("span");
+                    span.innerHTML = `${data.like} thích`
+
+                    buttonLike.classList.toggle("active");
+
+                }
+            });
+
+    });
+}
+// End Button Like
+
+// Button Favorite
+const buttonFavorite = document.querySelector("[button-favorite]");
+
+if (buttonFavorite) {
+    buttonFavorite.addEventListener("click", () => {
+        const idSong = buttonFavorite.getAttribute("button-favorite");
+        const isActive = buttonFavorite.classList.contains("active");
+
+        const typeFavorite = isActive ? "unfavorite" : "favorite";
+        const link = `/songs/favorite/${typeFavorite}/${idSong}`;
+
+        const option = {
+            method: "PATCH"
+        }
+        fetch(link, option)
+            .then(res => res.json())
+            .then(data => {
+                if (data.code == 200) {
+                    buttonFavorite.classList.toggle("active");
+
+                }
+
+            });
+
+
+    });
+}
+// End Button Favorite
